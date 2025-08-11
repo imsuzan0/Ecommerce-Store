b1597cf01bc2670c9e4fb2cacbf261f80921e49e
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-08-11 23:06:31 +0545

    feat: load environment variables using dotenv for Redis configuration

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,5 +1,9 @@
 import Redis from "ioredis"
+import { configDotenv } from "dotenv";
+
+configDotenv();
 
 export const redis = new Redis(process.env.UPSTASH_REDIS_URL,{
     tls:{}
 });
+// console.log("Redis URL:", process.env.UPSTASH_REDIS_URL);
