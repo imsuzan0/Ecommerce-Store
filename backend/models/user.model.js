0758567e48bad88557536316e25ddccc058d0654
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-08-11 20:21:00 +0545

    fix: move User model declaration to the end of the file for better readability

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -42,8 +42,6 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-const User = mongoose.model("User", userSchema);
-
 //Pre-save hook to hash password before saving to database
 userSchema.pre("save", async () => {
   if (!this.isModified("password")) return next();
@@ -60,5 +58,7 @@ userSchema.pre("save", async () => {
 userSchema.method.comparePassword = async () => {
   const isMatch = await bcrypt.compare(password, this.password);
 };
+
+const User = mongoose.model("User", userSchema);
 
 export default User;
