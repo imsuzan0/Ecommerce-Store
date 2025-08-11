05897977d7af387626508dc6d1f59b365af2b4bb
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-08-11 23:06:51 +0545

    fix: change auth routes to use POST method for signup, login, and logout

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -2,8 +2,8 @@ import express from "express";
 import { login, logout, signup } from "../controllers/auth.controller.js";
 const authRouter = express.Router();
 
-authRouter.get("/signup", signup);
-authRouter.get("/login", login);
-authRouter.get("/logout", logout);
+authRouter.post("/signup", signup);
+authRouter.post("/login", login);
+authRouter.post("/logout", logout);
 
 export default authRouter;
