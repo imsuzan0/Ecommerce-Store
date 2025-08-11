eb41b7908519f2055ccdf961c268d69a9bbf8748
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-08-11 23:07:03 +0545

    feat: implement signup functionality with user validation and token generation

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,77 @@
-export const signup = (req, res) => {
-  res.send("Signup route");
+import { redis } from "../lib/redis.js";
+import User from "../models/user.model.js";
+import { generateTokens } from "../utils/generateTokens.js";
+import { setCookies } from "../utils/setCookies.js";
+import jwt from "jsonwebtoken";
+
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    if (!name || !email || !password)
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    if (!emailRegex.test(email))
+      return res.status(400).json({ success: false, message: "Invalid email" });
+
+    const isUserExist = await User.findOne({ email });
+
+    if (isUserExist) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exist" });
+    }
+
+    const user = await User.create({ name, email, password });
+
+    const { accessToken, refreshToken } = generateTokens(user._id);
+
+    const storeRefreshToken = async (userId, refreshToken) => {
+      await redis.set(
+        `refresh_token:${userId}`,
+        refreshToken,
+        "EX",
+        7 * 24 * 60 * 60
+      );
+    };
+    await storeRefreshToken(user._id, refreshToken);
+
+    setCookies(res, accessToken, refreshToken);
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    console.log("Error in signup controller: ", error);
+    res.status(500).json({ success: false, message: "Something went wrong" });
+  }
 };
-export const login = (req, res) => {
-  res.send("Login route");
-}
-export const logout = (req, res) => {
-  res.send("Logout route");
-}
+
+export const login=async (req,res)=>{}
+
+
+
+export const logout = async(req, res) => {
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    if(refreshToken){
+      const decoded=jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET)
+      await redis.del(`refresh_token:${decoded.userId}`);
+    }
+    res.clearCookie("accessToken");
+    res.clearCookie("refreshToken");
+    res.status(200).json({ success: true, message: "User logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller: ", error);
+    res.status(500).json({ success: false, message: "Something went wrong" });
+  }
+};
