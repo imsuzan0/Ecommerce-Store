869a9fd55d51f34a5753e5c8e21fe77e0156fa0f
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-08-12 21:39:36 +0545

    fix:update import statements in auth routes

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import {
+  login,
+  logout,
+  refreshToken,
+  signup,
+  getProfile,
+} from "../controllers/auth.controller.js";
 const authRouter = express.Router();
 
 authRouter.post("/signup", signup);
